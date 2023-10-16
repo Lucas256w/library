705531df1e1d6e6c399119a0f0551bdad3aace13
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-10-16 15:00:59 -0400

    Implement modal pop up.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
+const addBook = document.querySelector('#add-book-button');
+const dialog = document.querySelector('dialog');
+const submitBtn = document.querySelector('#submit-button');
+const cancelBtn = document.querySelector('#cancel-button');
+
+addBook.addEventListener('click', () => {
+    dialog.showModal()
+});
+
+cancelBtn.addEventListener('click', e => {
+    e.preventDefault()
+    dialog.close()
+});
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,10 +21,11 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLirary() {
-    
-}
+// function addBookToLirary() {
 
-function diplayBooks {
-    for ( i = 0; i < myLibrary.length; i++)
-}
+// }
+
+// function diplayBooks {
+//     for ( i = 0; i < myLibrary.length; i++)
+// }
+
